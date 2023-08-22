5417ca7ce2c9af561279005c6ed7861b9b479140
Dasmanth <dasmanthkare@gmail.com>
2023-08-22 11:58:10 +0530

    Linux commands

--- 02.Print.sh
+++ 02.Print.sh
@@ -5,10 +5,9 @@ echo HELLO WORLD
 syntax : echo -e "\e[COLmMESSAGE\e[0m"
 # COL stands for color possible colors are RED(31) ,GREEN(32) ,YELLOW(33),BLUE(34) ,MAGENTA(35) ,CYAN(36)
 
-
-echo -e "\e[31 Dasamantha rao \e[0m"
-echo -e "\e[32 Dasamantha rao \e[0m"
-echo -e "\e[33 Dasamantha rao \e[0m"
-echo -e "\e[34 Dasamantha rao \e[0m"
-echo -e "\e[35 Dasamantha rao \e[0m"
-echo -e "\e[36 Dasamantha rao \e[0m"
+echo -e "\e[31mDasamantha rao \e[0m"
+echo -e "\e[32mDasamantha rao \e[0m"
+echo -e "\e[33mDasamantha rao \e[0m"
+echo -e "\e[34mDasamantha rao \e[0m"
+echo -e "\e[35mDasamantha rao \e[0m"
+echo -e "\e[36mDasamantha rao \e[0m"
